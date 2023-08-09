2d94be8c891676dcc9b9691e710a218d90297142
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-08-08 22:43:35 -0300

    feat: created and styled components
    created and styled header and contacts list components

--- fe/src/components/App/index.js
+++ fe/src/components/App/index.js
@@ -2,12 +2,20 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
+import { Container } from './styles';
+
+import Header from '../Header';
+import ContactsList from '../ContactsList';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>MyContacts</h1>
+      <Container>
+        <Header />
+        <ContactsList />
+      </Container>
+
     </ThemeProvider>
   );
 }
---
+++ fe/src/components/ContactsList/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 32px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+
+  strong {
+    color: #222;
+    font-size: 24px;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    border-radius: 4px;
+    padding: 8px 16px;
+    transition: all 0.2s ease-in;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.primary.main};
+      color: #fff;
+    }
+  }
+`;
