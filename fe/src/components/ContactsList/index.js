0eb7e6bc1a49aa8e27f20c6e43f319b0aea6a6d7
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-08-09 08:43:15 -0300

    feat: developed list contacts component

--- fe/src/components/ContactsList/index.js
+++ fe/src/components/ContactsList/index.js
@@ -1,4 +1,9 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +12,102 @@ export default function ContactsList() {
         <strong>3 contacts</strong>
         <a href="/">New Contact</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>
+                Name Lastname
+              </strong>
+              <small>
+                Category
+              </small>
+            </div>
+
+            <span>
+              email@email.com
+            </span>
+            <span>
+              (41) 99999-9999
+            </span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>
+                Name Lastname
+              </strong>
+              <small>
+                Category
+              </small>
+            </div>
+
+            <span>
+              email@email.com
+            </span>
+            <span>
+              (41) 99999-9999
+            </span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>
+                Name Lastname
+              </strong>
+              <small>
+                Category
+              </small>
+            </div>
+
+            <span>
+              email@email.com
+            </span>
+            <span>
+              (41) 99999-9999
+            </span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
