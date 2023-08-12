8e2397721526180b0323c0df3cd930a43e574334
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-08-11 22:26:53 -0300

    feat: new components select and input

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,7 +1,30 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader title="New contact" />
+    <>
+      <PageHeader
+        title="New contact"
+      />
+      <Input
+        type="text"
+        placeholder="Name"
+      />
+      <Input
+        type="text"
+        placeholder="E-mail"
+      />
+      <Input
+        type="text"
+        placeholder="Phone"
+      />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">X</option>
+        <option value="123">TikTok</option>
+      </Select>
+    </>
   );
 }
