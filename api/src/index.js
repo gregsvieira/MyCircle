2ba845712b068aae5c2350dc73edd041f534195b
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-08-27 14:11:05 -0300

    refactor: errorHandler function to middleware

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('express-async-errors');
 
 const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
@@ -9,14 +10,8 @@ const app = express();
 const port = 3001;
 app.use(express.json());
 app.use(cors);
-
 app.use(routes);
-
-// Error Handler do Express
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
