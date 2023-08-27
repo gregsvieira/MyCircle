3fe9e88952a3ed01cfbf881b2b9e0c840e3ca4c8
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-08-27 17:25:06 -0300

    feat: rendering components based on fetched data

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -21,15 +21,17 @@ export default function Home() {
       });
   }, []);
 
-  console.log(contacts);
-
   return (
     <Container>
       <InputSearchContainer>
         <input type="text" placeholder="Search contact..." />
       </InputSearchContainer>
       <Header>
-        <strong>3 contacts</strong>
+        <strong>
+          {contacts.length}
+          {' '}
+          {contacts.length === 1 ? 'contact' : 'contacts'}
+        </strong>
         <Link to="/new">New Contact</Link>
       </Header>
 
@@ -41,34 +43,39 @@ export default function Home() {
           </button>
         </header>
 
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>
-                Name Lastname
-              </strong>
-              <small>
-                Category
-              </small>
+        {contacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="info">
+              <div className="contact-name">
+                <strong>
+                  {contact.name}
+                </strong>
+                {contact.category_name && (
+                  <small>
+                    {contact.category_name}
+                  </small>
+                )}
+              </div>
+
+              <span>
+                {contact.email}
+              </span>
+              <span>
+                {contact.phone}
+              </span>
             </div>
 
-            <span>
-              email@email.com
-            </span>
-            <span>
-              (41) 99999-9999
-            </span>
-          </div>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={edit} alt="edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="delete" />
+              </button>
+            </div>
+          </Card>
+        ))}
 
-          <div className="actions">
-            <Link to={`/edit/${123}`}>
-              <img src={edit} alt="edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="delete" />
-            </button>
-          </div>
-        </Card>
       </ListContainer>
     </Container>
   );
