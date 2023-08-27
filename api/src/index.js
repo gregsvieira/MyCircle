40fb941262761ed411309c63a6397be5c655e381
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-08-27 14:07:47 -0300

    feat: middleware cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
 const routes = require('./routes');
 
 const app = express();
 
-const port = 3333;
+const port = 3001;
 app.use(express.json());
+app.use(cors);
+
 app.use(routes);
 
 // Error Handler do Express
