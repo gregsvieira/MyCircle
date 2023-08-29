b3ab3b95f2b0db131201a760589320e8c58259dc
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-08-28 21:35:33 -0300

    feat: search name

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -11,6 +11,11 @@ import trash from '../../assets/images/icons/trash.svg';
 export default function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderyBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredContacts = contacts.filter((contact) => (
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+  ));
 
   useEffect(() => {
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
@@ -28,28 +33,39 @@ export default function Home() {
     );
   }
 
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <Container>
       <InputSearchContainer>
-        <input type="text" placeholder="Search contact..." />
+        <input
+          value={searchTerm}
+          type="text"
+          placeholder="Search contact..."
+          onChange={handleChangeSearchTerm}
+        />
       </InputSearchContainer>
       <Header>
         <strong>
-          {contacts.length}
+          {filteredContacts.length}
           {' '}
-          {contacts.length === 1 ? 'contact' : 'contacts'}
+          {filteredContacts.length === 1 ? 'contact' : 'contacts'}
         </strong>
         <Link to="/new">New Contact</Link>
       </Header>
 
-      <ListHeader orderBy={orderBy}>
-        <button type="button" className="sort-button" onClick={handleOrderBy}>
-          <span>Nome</span>
-          <img src={arrow} alt="arrow" />
-        </button>
-      </ListHeader>
+      {filteredContacts.length > 0 && (
+        <ListHeader orderBy={orderBy}>
+          <button type="button" className="sort-button" onClick={handleOrderBy}>
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </ListHeader>
+      )}
 
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
