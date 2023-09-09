ef6056cc3642e172bd062852fbc9e5e3a28b8867
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-09-09 14:40:38 -0300

    refactor: button component add spinner and block

--- fe/src/components/Select.js
+++ fe/src/components/Select.js
@@ -23,5 +23,7 @@ export default styled.select`
   &[disabled] {
     background-color: ${({ theme }) => theme.colors.gray[100]};
     border-color: ${({ theme }) => theme.colors.gray[200]};
+    opacity: 1;
+    color: #cbc0bc;
   }
 `;
