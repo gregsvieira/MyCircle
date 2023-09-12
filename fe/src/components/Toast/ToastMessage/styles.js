ec01fa985683e9abc33213619be61fbf57fb3cc0
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-09-11 21:26:13 -0300

    feat: remove toast onclick

--- fe/src/components/Toast/ToastMessage/styles.js
+++ fe/src/components/Toast/ToastMessage/styles.js
@@ -17,6 +17,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
 
   ${({ type }) => containerVariants[type] || containerVariants.default}
 
