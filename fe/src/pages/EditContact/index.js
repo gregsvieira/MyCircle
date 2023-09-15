47418ee03c8c6ff3aa01e9b5ead78d5260408942
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-09-15 08:59:21 -0300

    feat: load contact list and history on catch

--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -1,13 +1,54 @@
-import ContactForm from '../../components/ContactForm';
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+
+import toast from '../../utils/toast';
+
+import ContactsService from '../../services/ContactsService';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        setIsLoading(false);
+        console.log({ contactData });
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contact not found!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader
         title="Edit contact"
       />
-      <ContactForm title="Edit contact" buttonLabel="Save changes" />
+      <ContactForm
+        buttonLabel="Save changes"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
