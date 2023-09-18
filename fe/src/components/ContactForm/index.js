ff0882b637bdce88dba8530ebd6df4ee55e28a25
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-09-17 21:13:04 -0300

    refactor: change useEffect to useImperativeHandle

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect, forwardRef } from 'react';
+import {
+  useState, useEffect, forwardRef, useImperativeHandle,
+} from 'react';
 
 import isEmailValid from '../../utils/isEmailValid';
 import formatPhone from '../../utils/formatPhone';
@@ -28,17 +30,14 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
 
   const isFormValid = (name && errors.length === 0);
 
-  useEffect(() => {
-    const refObject = ref;
-    refObject.current = {
-      setFieldsValues: (contact) => {
-        setName(contact.name);
-        setEmail(contact.email);
-        setPhone(contact.phone);
-        setCategoryId(contact.category_id);
-      },
-    };
-  }, [ref]);
+  useImperativeHandle(ref, () => ({
+    setFieldsValues: (contact) => {
+      setName(contact.name);
+      setEmail(contact.email);
+      setPhone(contact.phone);
+      setCategoryId(contact.category_id);
+    },
+  }), []);
 
   useEffect(() => {
     async function loadCategories() {
