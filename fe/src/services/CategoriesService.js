b9a5348321b6d1c96c36f42a4702e9f07705b4c7
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-09-25 06:56:25 -0300

    feat: new methods on client

--- fe/src/services/CategoriesService.js
+++ fe/src/services/CategoriesService.js
@@ -5,12 +5,25 @@ class CategoriesService {
     this.httpClient = new HttpClient('http://localhost:3001');
   }
 
-  listCategories() {
-    return this.httpClient.get('/categories');
+  listCategories(orderBy = 'asc') {
+    return this.httpClient.get(`/categories?orderBy=${orderBy}`);
+  }
+
+  getCategoryById(id) {
+    return this.httpClient.get(`/categories/${id}`);
   }
 
   createCategory(name) {
     return this.httpClient.post('/categories', { body: name });
+  }
+
+  updateCategory(id, category) {
+    console.log(category);
+    return this.httpClient.put(`/categories/${id}`, { body: category });
+  }
+
+  deleteCategory(id) {
+    return this.httpClient.delete(`/categories/${id}`);
   }
 }
 
