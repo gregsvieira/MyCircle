37168344b7da237e5b72af27fb017c515e6e9d64
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-11-25 18:37:08 -0300

    refactor: remove unused routes

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -5,15 +5,10 @@ import UploadController from '../src/app/controllers/UploadController';
 
 const router = Router();
 
-router.get(
-    '/contacts',
-    ContactController.index,
-);
-
+router.get('/contacts',  ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts/', ContactController.store);
-router.post('/contacts/batch', ContactController.batchStoreContacts);
 router.put('/contacts/:id', ContactController.update);
 
 router.get('/categories', CategoryController.index);
