785494c39be0541ae8a8bc39114562ebc54f0bc8
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-11-24 22:03:05 -0300

    refactor: index to typescript

---
+++ api/src/index.ts
@@ -0,0 +1,18 @@
+import express, { json } from 'express';
+import 'express-async-errors';
+
+import cors from './app/middlewares/cors';
+import errorHandler from './app/middlewares/errorHandler';
+import routes from './routes';
+
+const app = express();
+
+const port = 3001;
+app.use(json());
+app.use(cors);
+app.use(routes);
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`server running on http://localhost:${port}`);
+});
