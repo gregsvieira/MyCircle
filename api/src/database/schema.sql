6f48cbc35cf3e23fa07e200747521625a3fd9af3
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-11-25 17:35:33 -0300

    feat: new tables for add functionality
    create status, roles, users and users_tokens

--- api/src/database/schema.sql
+++ api/src/database/schema.sql
@@ -1,19 +1,55 @@
-CREATE DATABASE mycontacts;
+-- CREATE DATABASE mycontacts;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS status (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS categories (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS roles (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL UNIQUE,
+  username VARCHAR NOT NULL UNIQUE,
+  phone VARCHAR UNIQUE,
+  password VARCHAR NOT NULL,
+  active BOOLEAN DEFAULT true NOT NULL,
+  role_id UUID NOT NULL,
+  FOREIGN KEY(role_id) REFERENCES roles(id)
+);
+
+CREATE TABLE IF NOT EXISTS users_tokens (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  access_token VARCHAR NOT NULL,
+  refresh_token VARCHAR NOT NULL,
+  expires_date DATE NOT NULL,
+  user_id UUID NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
 CREATE TABLE IF NOT EXISTS contacts (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL,
-  email VARCHAR UNIQUE,
+  email VARCHAR UNIQUE NOT NULL,
   phone VARCHAR,
+  active BOOLEAN DEFAULT false,
   category_id UUID,
-  FOREIGN KEY(category_id) REFERENCES categories(id)
+  status_id UUID,
+  user_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id),
+  FOREIGN KEY(status_id) REFERENCES status(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 ALTER TABLE contacts
