489fce3e8f19d5baf9b4e46179dba1beb600a7a2
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-11-24 22:04:15 -0300

    refactor: utils to typescript

--- api/src/app/utils/getErrorMessage.ts
+++ api/src/app/utils/getErrorMessage.ts
@@ -1,4 +1,4 @@
-module.exports = function getErrorMessage(fields, singular, message) {
+export default function getErrorMessage(fields: any[], singular: any, message: any) {
   const field = fields.length > 1 ? 'Fields' : 'Field';
   return `${field} ${fields.join(', ')} ${message}`;
 };
