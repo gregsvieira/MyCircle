2c238e395046c6b6505df50b1f9a129207ec352a
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-01-25 18:47:34 -0300

    feat: navbar componente

---
+++ fe/src/components/NavBar/index.js
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import PropTypes from 'prop-types';
+
+import { AiOutlineMenu } from 'react-icons/ai';
+import { useLocation } from 'react-router-dom';
+import SideBar from '../SideBar';
+import ToggleThemeButton from '../ToggleThemeButton';
+import { Container } from './styles';
+// import Avatar from '../Avatar';
+
+export default function NavBar({ theme, onToggleTheme }) {
+  const [openSideBar, setOpenSideBar] = useState(false);
+  const location = useLocation();
+
+  const handleShowSideBar = useCallback(() => {
+    setOpenSideBar((prevState) => !prevState);
+  }, []);
+
+  useEffect(() => {
+    setOpenSideBar(false);
+  }, [location]);
+
+  return (
+    <Container>
+      <AiOutlineMenu size={32} onClick={handleShowSideBar} />
+      <ToggleThemeButton theme={theme} onToggleTheme={onToggleTheme} />
+      {/* <Avatar isActive name="AD" size="s" onClick={handleShowSideBar} /> */}
+
+      {openSideBar && <SideBar onShowSideBar={handleShowSideBar} />}
+    </Container>
+  );
+}
+
+NavBar.propTypes = {
+  theme: PropTypes.string.isRequired,
+  onToggleTheme: PropTypes.func.isRequired,
+};
