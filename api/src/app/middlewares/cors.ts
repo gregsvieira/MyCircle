128151040413b76d7e252063ff47748e0f30285d
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-01-25 18:30:57 -0300

    refactor: update credentials on cors

--- api/src/app/middlewares/cors.ts
+++ api/src/app/middlewares/cors.ts
@@ -4,6 +4,7 @@ export default (request: Request, response: Response, next: NextFunction) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.setHeader('Access-Control-Allow-Credentials', 'true');
   response.setHeader('Access-Control-Allow-Max-Age', '10');
   next();
 };
