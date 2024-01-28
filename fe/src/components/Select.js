a1ddbfd05342afd23214a0795f90731d34e4f5b0
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-01-27 23:14:37 -0300

    refactor: change styles to use mode

--- fe/src/components/Select.js
+++ fe/src/components/Select.js
@@ -3,15 +3,16 @@ import styled from 'styled-components';
 export default styled.select`
   width: 100%;
   height: 52px;
-  background: #fff;
+  background: ${({ theme }) => theme.mode.inputColor};
   box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04);
   border: none;
-  border: 2px solid #fff;
+  border: 2px solid ${({ theme }) => theme.mode.inputColor};
   border-radius: 4px;
   outline: 0;
   padding: 0 16px;
   transition: border-color 0.2s ease-in;
   appearance: none;
+  color: ${({ theme }) => theme.mode.selectTextColor};
 
   &:focus {
     border-color: ${({ theme }) => theme.colors.primary.main};
